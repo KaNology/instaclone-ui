01cd46e2eec8ee44c8d5fb522ec802491e032f39
Le Thien An <an.le091000@gmail.com>
2022-10-03 21:37:37 +0700

    PostPhotoPreview

--- src/views/Post/CreatePostView.vue
+++ src/views/Post/CreatePostView.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container" style="background-color: white;">
         <div class="row">
-            <h2>New Photo</h2>
+            <h2>New Post</h2>
         </div>
         <form action="">
             <div class="form-row mb-5">
@@ -26,12 +26,28 @@
                     </div>
                 </div>
             </div>
-            <div class="form-row">
-                <div class="form-group">
-                    <label>Photo/Video</label>
-                    <input type="file" class="form-control" accept="image/png, image/jpeg, image/jpg">
+            <div class="form-group">
+                <label>Photo/Video</label>
+
+                <div class="row">
+                    <div v-for="(image,key) in previewImages" :key="key" class="image-box col-3">
+                        <img :src="image" class="image-preview">
+                        <div class="image-layer">
+                            <i @click="removeImage(image)" class="image-close bi bi-x-circle"></i>
+                        </div>
+                    </div>
+                    <div class="col-3">
+                        <label class="image-label" for="file">
+                            <div class="image-icon">
+                                <i class="bi bi-plus-circle"></i>
+                            </div>
+                        </label>
+                        <input style="display: none;" @change="onFileChange" id="file" type="file" ref="uploadImage"
+                            class="form-control" accept="image/png, image/jpeg, image/jpg" multiple>
+                    </div>
                 </div>
             </div>
+
             <div class="form-row">
                 <button type="submit" class="btn btn-success">Create</button>
             </div>
@@ -44,7 +60,28 @@ export default {
     data() {
         return {
             id: null,
+            previewImages: [],
+            // active: false,
         };
+    },
+    methods: {
+        onFileChange() {
+            let files = this.$refs.uploadImage.files;
+            if (!files.length)
+                return;
+
+            for (let i = 0; i < files.length; i++) {
+                let reader = new FileReader
+                reader.onload = e => {
+                    this.previewImages.push(e.target.result)
+                }
+                reader.readAsDataURL(files[i])
+            }
+        },
+        removeImage(object) {
+            const filtersList = this.previewImages.filter(element => element !== object)
+            this.previewImages = filtersList
+        }
     },
     mounted() {
         this.id = this.$route.params.id;
@@ -53,5 +90,51 @@ export default {
 }
 </script>
 <style>
+.image-preview {
+    width: 200px;
+    height: 200px;
+    display: block;
+    margin: 10px 10px;
+    object-fit: cover;
+}
 
+.image-layer {
+    width: 200px;
+    height: 200px;
+    display: block;
+    margin: 10px 10px;
+
+    color: white;
+    font-size: 30px;
+
+    position: absolute;
+    top: 0px;
+
+    background-color: black;
+    opacity: 0;
+}
+
+.image-box:hover>.image-layer {
+    opacity: 0.5;
+}
+
+.image-close {
+    position: absolute;
+    right: 10px;
+
+    cursor: pointer;
+}
+
+.image-icon {
+    display: block;
+    width: 200px;
+    height: 200px;
+    margin: 10px 10px;
+
+    color: white;
+    text-align: center;
+    font-size: 125px;
+    background-color: black;
+    opacity: calc(50%);
+}
 </style>
